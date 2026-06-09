af3361f80d0e98e23e58fec725d50568d4ac7f60
daferali20 <31751340+daferali20@users.noreply.github.com>
2026-06-09 18:36:16 +0300

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Dashboard from "./components/Dashboard";
+
+import "./styles/dashboard.css";
+
+export default function App() {
+
+  return (
+
+    <>
+      <Header />
+      <Dashboard />
+    </>
+
+  );
+
+}
